4ece037a37f53cd0cb4417194ebe75062a206bde
Rojin <ro_jin25@icloud.com>
2018-02-26 10:57:54 -0800

    2.4

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-var firstnumber = document.getElementById("num1").value;
-var secoundnumber = document.getElementById("num1").value;
+
 function multiply() {
+  var firstnumber = document.getElementById("num1").value;
+  var secoundnumber = document.getElementById("num2").value;
   console.log(firstnumber, secoundnumber);
   var product = Number.parseInt(firstnumber) * Number.parseInt(secoundnumber);
-  console.log(firstnumber, secoundnumber, product);
+  var add = Number.parseInt(firstnumber) + Number.parseInt(secoundnumber);
+  var sub = Number.parseInt(firstnumber) - Number.parseInt(secoundnumber);
+  var div = Number.parseInt(firstnumber) / Number.parseInt(secoundnumber);
+  console.log(firstnumber, secoundnumber, product, add, sub, div);
+  document.getElementById("product").value = product;
+  document.getElementById("add").value = add;
+  document.getElementById("sub").value = sub;
+  document.getElementById("div").value = div;
 }
-// console.log("hello");
-//multiply
-// var name, name2;
-// var result;
-// result = name * name2;
