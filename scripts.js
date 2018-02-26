8d152bc3911bf58d0a76376489a8cc0555d9597c
Rojin <ro_jin25@icloud.com>
2018-02-26 11:44:32 -0800

    main 2.5

--- scripts.js
+++ scripts.js
@@ -13,3 +13,39 @@ function math() {
   document.getElementById("sub").value = sub;
   document.getElementById("div").value = div;
 }
+
+function add() {
+  var firstnumber = document.getElementById("num1").value;
+  var secoundnumber = document.getElementById("num2").value;
+  var add = Number.parseInt(firstnumber) + Number.parseInt(secoundnumber);
+  console.log(firstnumber, secoundnumber, add);
+  document.getElementById("add").value = add;
+
+}
+
+function product() {
+  var firstnumber = document.getElementById("num1").value;
+  var secoundnumber = document.getElementById("num2").value;
+  var product = Number.parseInt(firstnumber) * Number.parseInt(secoundnumber);
+  console.log(firstnumber, secoundnumber, product);
+  document.getElementById("product").value = product;
+
+}
+
+function sub() {
+  var firstnumber = document.getElementById("num1").value;
+  var secoundnumber = document.getElementById("num2").value;
+  var sub = Number.parseInt(firstnumber) - Number.parseInt(secoundnumber);
+  console.log(firstnumber, secoundnumber, sub);
+  document.getElementById("sub").value = sub;
+
+}
+
+function div() {
+  var firstnumber = document.getElementById("num1").value;
+  var secoundnumber = document.getElementById("num2").value;
+  var div = Number.parseInt(firstnumber) / Number.parseInt(secoundnumber);
+  console.log(firstnumber, secoundnumber, div);
+  document.getElementById("div").value = div;
+
+}
